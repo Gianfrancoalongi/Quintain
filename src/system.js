a75b26d9e0157be2451f9ef3c1898dd0baa4cbb5
Gianfranco Alongi <egiaalo@esekiws5098.rnd.ki.sw.ericsson.se>
2013-08-23 16:04:39 +0200

    system is now using the edge set implementation

--- src/system.js
+++ src/system.js
@@ -1,12 +1,13 @@
 function new_system() {
     var system = {
-	edges: [{id:1,from:'stdin',to:'device_1'}],
-	add_node_between: function(node) {
-	    var edge = this.edges[0];
-	    var new_edge = {id:2, from:node.id, to:edge.to};
-	    edge.to = node.id;
-	    this.edges[1] = new_edge;
+	edges: new_edge_set(),
+	add_node_between: function(id) {
+	    var edge = this.edges.get_edge(0);
+	    var oldto = edge.to;
+	    edge.to = id;
+	    this.edges.add_new_edge(id, oldto);
 	}
     };  
+    system.edges.add_new_edge('stdin', 'device_1');
     return system;
 }
--- src/tests.js
+++ src/tests.js
@@ -10,42 +10,39 @@ test("new stdin can be created", function() {
 
 test("new system has only input to stdout edge", function() {
     var system = new_system();
-    deepEqual( system.edges, [{id:1,from:'stdin',to:'device_1'}]);
+    deepEqual( system.edges.get_edge(0), {id:0, from:'stdin', to:'device_1'});
 });
 
 test("adding branch to empty system will put it between stdin and device", function() {
     var system = new_system();
     var branch = new_branch();
-    system.add_node_between(branch);
-    deepEqual( system.edges, [{id:1,from:'stdin',to:'branch_1'},
-			      {id:2,from:'branch_1',to:'device_1'}
-			     ]);
+    system.add_node_between(branch.id);
+    deepEqual( system.edges.get_edge(0), {id:0, from:'stdin', to:'branch_1'});
+    deepEqual( system.edges.get_edge(1), {id:1, from:'branch_1', to:'device_1'});
 });
 
 test("adding replicator to empty system will put it between stdin and device", function() {
     var system = new_system();
     var replicator = new_replicator();
-    system.add_node_between(replicator);
-    deepEqual( system.edges, [{id:1,from:'stdin',to:'replicator_1'},
-			     {id:2,from:'replicator_1',to:'device_1'}
-			    ]);
+    system.add_node_between(replicator.id);
+    deepEqual( system.edges.get_edge(0), {id:0,from:'stdin',to:'replicator_1'});
+    deepEqual( system.edges.get_edge(1), {id:1,from:'replicator_1',to:'device_1'});
 });
 
 test("adding splitter to empty system will put it between stdin and device", function() {
     var system = new_system();
     var splitter = new_splitter();
-    system.add_node_between(splitter);
-    deepEqual( system.edges, [{id:1,from:'stdin',to:'splitter_1'},
-			      {id:2,from:'splitter_1',to:'device_1'}]);
+    system.add_node_between(splitter.id);
+    deepEqual( system.edges.get_edge(0), {id:0,from:'stdin',to:'splitter_1'});
+    deepEqual( system.edges.get_edge(1), {id:1,from:'splitter_1',to:'device_1'});
 });
 
 test("adding function to empty system will put it between stdin and device", function() {
     var system = new_system();
     var funct = new_function();
-    system.add_node_between(funct);
-    deepEqual( system.edges, [{id:1,from:'stdin',to:'function_1'},
-			      {id:2,from:'function_1',to:'device_1'}
-			     ]);
+    system.add_node_between(funct.id);
+    deepEqual( system.edges.get_edge(0), {id:0,from:'stdin',to:'function_1'});
+    deepEqual( system.edges.get_edge(1), {id:1,from:'function_1',to:'device_1'});
 });
 
 test("edge handler can create empty edge set", function() {
